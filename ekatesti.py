9db7e0cb46100f3a9f467474924f1a47f8fa4254
jingping1 <jingp@metropolia.fi>
2026-01-18 00:16:10 +0200

    Add files via upload

---
+++ ekatesti.py
@@ -0,0 +1,12 @@
+print("Hello World")
+print("Tämän koodasin itse,jee!")
+
+#kysytään käyttäjältä tämä nimi,
+#otetaan saatu vastaus talteen muuttyjaan,
+#jonka nimenä on nimi.
+nimi=input("mikä on nimesi?")
+
+# Shift 2 , will get "
+# Shift right 4, will get ;
+
+
